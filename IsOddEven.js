d28c60cba4970fe911b7a71e0f2ec3a69e50bb18
MonicaV <mailme.monii@gmail.com>
2026-05-27 02:33:07 +0530

    Add files via upload

---
+++ IsOddEven.js
@@ -0,0 +1,12 @@
+let number= 2
+function isOddEven(number)
+{
+    if(number%2==0)
+    {
+        console.log("Given Number "+number+" is even")
+     
+    }
+    else
+        console.log("Given Number "+number+" is odd")
+}
+isOddEven(8);
